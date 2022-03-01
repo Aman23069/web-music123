50727cda09c64084a36aa9f48cd44ed606835cc8
Aman23069 <58472535+Aman23069@users.noreply.github.com>
2022-03-01 14:52:43 +0530

    Create urls.py

--- music/urls.py
+++ music/urls.py
@@ -24,7 +24,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.static import url
+from django.conf.urls import url
 from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
