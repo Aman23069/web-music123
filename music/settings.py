92c65569044a141905ac09e8d051e4c922d60ba8
Aman23069 <58472535+Aman23069@users.noreply.github.com>
2022-03-06 12:06:36 +0530

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -111,11 +111,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/musicbeats/static/'
-STATICFILES_DIRS = [
-    "/musicbeats/static/musicbeats/"
-]
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+#STATIC_URL = '/musicbeats/static/'
+
+#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'musicbeats/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(_file_))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'musicbeats/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,  "musicbeats/static"),
+]
